bf6fc361c5c0910d31582eb497e563bbc4077e06
Aleksei <agsfinks@gmail.com>
2023-02-07 21:10:34 +0000

    aleks example

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const currentYear = new Date().getFullYear();
-const copyright = document.getElementById("copyright");
-copyright.innerHTML = "Website design and Implemented by Jack Griffin Copyright &copy; " + currentYear;
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    const currentYear = new Date().getFullYear();
+    const copyright = document.querySelector("#copyright");
+    copyright.innerHTML = "Website design and Implemented by Jack Griffin Copyright &copy; " + currentYear;
+});
