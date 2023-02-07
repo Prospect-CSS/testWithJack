2207787a54f411a789c5e1f809c21dd362867ebd
griffin1995 <51092224+griffin1995@users.noreply.github.com>
2023-02-07 13:51:50 +0000

    commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+const copyright = document.getElementById("copyright");
+copyright.innerHTML = "Website design and Implemented by Jack Griffin Copyright &copy; " + currentYear;
